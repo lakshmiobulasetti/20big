07aed09683e71885fb867ddbddf4e91bde7e3bcc
lakshmiobulasetti <41731019+lakshmiobulasetti@users.noreply.github.com>
2018-09-21 20:11:05 +0530

    Update and rename README.md to 20.java

---
+++ 20.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int i = 0;
+      int n =0 ;
+
+      Scanner input = new Scanner(System.in);
+
+         
+         i = input.nextInt();
+         
+         n = input.nextInt();
+
+         if ((i <= 1) || (n <= 1)) {
+           
+         }
+
+         System.out.print(n + " multiples of " + i + " are: ");
+
+	}
+}
